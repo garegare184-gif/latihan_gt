4e64ce82c9ae62e10f5588679707a2317f34c4a6
Akasshy <akasshy01@gmail.com>
2025-07-29 14:20:00 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 Route::get('/HOME', function () {
     return view('welcome');
 });
