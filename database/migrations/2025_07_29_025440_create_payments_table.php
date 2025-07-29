d82053cac0b260131d54e804412006256ba6489d
dedenzulkipli <dedenzulkipli6@gmail.com>
2025-07-29 10:56:27 +0700

    indah

---
+++ database/migrations/2025_07_29_025440_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('iduser')->constrained('users')->onDelete('cascade');
+            $table->string('period');
+            $table->integer('nominal');
+            $table->string('petugas'); // nama petugas
+            $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
